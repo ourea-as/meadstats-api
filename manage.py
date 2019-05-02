e66633baaeb4b04ef355a9f64c33dce1f177727b
Fredrik Bore <fredrik@bore.ai>
2019-05-02 21:44:42 +0200

    Update packages

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app
@@ -7,8 +7,8 @@ from app.models import db
 migrate = Migrate(app, db)
 
 manager = Manager(app)
-manager.add_command("db", MigrateCommand)
-
+manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server())
 
 if __name__ == '__main__':
     manager.run()
