bd3fc6e8ba53b0203053f2833fd2a9a0dd302389
Fredrik Bore <fredrik@bore.ai>
2019-08-23 23:55:13 +0200

    Temp fix

--- app/app.py
+++ app/app.py
@@ -176,14 +176,10 @@ def create_app():
         user = get_user_from_db(username)
 
         if user:
-            print(user)
-            userdata = user_schema.dump(user, many=False)
-            print(userdata)
-
             data = {
                 "status": "success",
                 "data": {
-                    "user": user_schema.dump(user, many=False)[0]
+                    "user": user_schema.dump(user, many=False)
                 }
             }
 
